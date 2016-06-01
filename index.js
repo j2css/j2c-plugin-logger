709286c84316a2a961faa3540ac23fd6b99f2026
Pierre-Yves Gerardy <py@py.gy>
2016-06-01 18:38:40 +0200

    return the final value in the `x` handler

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function logger(name) {
   return {$filter: function(next) {
     return {
       i: function() {console.log(name, 'i', [].slice.call(arguments)); next.i.apply(next, arguments)},
-      x: function() {console.log(name, 'x', [].slice.call(arguments)); next.x.apply(next, arguments)},
+      x: function() {console.log(name, 'x', [].slice.call(arguments)); return next.x.apply(next, arguments)},
       a: function() {console.log(name, 'a', [].slice.call(arguments)); next.a.apply(next, arguments)},
       A: function() {console.log(name, 'A', [].slice.call(arguments)); next.A.apply(next, arguments)},
       d: function() {console.log(name, 'd', [].slice.call(arguments)); next.d.apply(next, arguments)},
